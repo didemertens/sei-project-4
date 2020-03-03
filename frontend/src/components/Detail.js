65ee99a173d0168fec6663b12ba8659928310ab3
didemertens <d.g.j.mertens@gmail.com>
2020-03-03 15:15:24 +0000

    register in hooks

--- frontend/src/components/Detail.js
+++ frontend/src/components/Detail.js
@@ -4,22 +4,14 @@ import { Link } from 'react-router-dom'
 import moment from 'moment'
 import Auth from './lib/Auth'
 import { TiDelete, TiArrowLeftThick } from "react-icons/ti"
-
 import SunEditor from "suneditor-react"
 import 'suneditor/dist/css/suneditor.min.css'
 import parse from 'html-react-parser'
-
 
 const Detail = ({ history, match }) => {
   const [currentUser, setUser] = useState('')
   const [question, setQuestion] = useState({})
   const [answerData, setAnswer] = useState({})
-
-  const getUser = () => {
-    const currentUser = Auth.getPayload().sub
-    setUser(currentUser)
-    getQuestion()
-  }
 
   const getQuestion = async () => {
     try {
@@ -69,6 +61,11 @@ const Detail = ({ history, match }) => {
   }
 
   useEffect(() => {
+    const getUser = () => {
+      const currentUser = Auth.getPayload().sub
+      setUser(currentUser)
+      getQuestion()
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     getUser()
   }, [])
@@ -164,168 +161,5 @@ const Detail = ({ history, match }) => {
     </div >
   )
 }
-
-// class Detail extends React.Component {
-//   state = {
-//     currentUser: '',
-//     question: {
-//       owner: {},
-//       answers: [],
-//       languages: [],
-//       title: '',
-//       text: ''
-//     },
-//     answerData: {
-//       text: '',
-//       submitted: false
-//     }
-//   }
-
-// getQuestion = async () => {
-//   try {
-//     const { data } = await axios.get(`/api/questions/${this.props.match.params.id}`)
-//     this.setState({ question: data })
-//     const answerData = { ...this.state.answerData, submitted: false }
-//     this.setState({ answerData })
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
-
-// handleChangeEditor = (content) => {
-//   const answerData = { ...this.state.answerData, text: content }
-//   this.setState({ answerData })
-// }
-
-// handleSubmit = async (e) => {
-//   e.preventDefault()
-//   try {
-//     await axios.post(`/api/questions/${this.props.match.params.id}/answers/`, this.state.answerData,
-//       { headers: { Authorization: `Bearer ${Auth.getToken()}` } }
-//     )
-//     const answerData = { ...this.state.answerData, text: '', submitted: true }
-//     this.setState({ answerData })
-//     this.getQuestion()
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
-
-// handleDeleteQuestion = async (e) => {
-//   try {
-//     await axios.delete(`/api/questions/${this.state.question.id}/`,
-//       { headers: { Authorization: `Bearer ${Auth.getToken()}` } })
-//     this.props.history.push('/questions')
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
-
-// handleDeleteAnswer = async (answerId) => {
-//   try {
-//     await axios.delete(`/api/questions/${this.state.question.id}/answers/${answerId}/`,
-//       { headers: { Authorization: `Bearer ${Auth.getToken()}` } })
-//     this.getQuestion()
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
-
-// componentDidMount() {
-//   const currentUser = Auth.getPayload().sub
-//   this.setState({ currentUser })
-//   this.getQuestion()
-// }
-
-// render() {
-//   const { question, answerData, currentUser } = this.state
-//   return (
-//     <div className="container detail-container">
-//       <div className="columns">
-//         <div className="column is-8 is-offset-2">
-//           <div className="section">
-//             <div className="columns">
-//               <div className="column">
-//                 <Link to='/questions' className="button is-warning"><TiArrowLeftThick /> Questions</Link>
-//               </div>
-//               {question.owner.id === currentUser ?
-//                 <div className="column is-offset-8">
-//                   <div className="detail-delete">
-//                     <TiDelete onClick={this.handleDeleteQuestion} className="detail-delete-answer"></TiDelete>
-//                     <p className="is-size-7 detail-delete-text">Delete question</p>
-//                   </div>
-//                 </div>
-//                 :
-//                 null
-//               }
-//             </div>
-//             <h1 className="title">{question.title}</h1>
-//             <p className="subtitle is-size-6">{moment(question.created_at).calendar()} by <Link className="detail-link-user" to={`/profile/${question.owner.id}`}>
-//               {question.owner.username}</Link></p>
-//             <div className="detail-languages">
-//               {question.languages.map(language => (
-//                 <div key={language.id}>
-//                   <img className="image is-24x24" src={language.image} alt="{language.name}" />
-//                 </div>
-//               ))}
-//             </div>
-//             <div className="detail-text">{parse(question.text)}</div>
-//           </div>
-//           {/* answers */}
-//           {question.answers.length > 0 &&
-//             <>
-//               <div className="section">
-//                 <h5 className="title is-size-5">Answers:</h5>
-//                 {question.answers.map(answer => {
-//                   return (
-//                     <div className="box" key={answer.id}>
-//                       <div className="has-text-right">
-//                         {answer.owner.id === currentUser &&
-
-//                           <div className="detail-delete">
-//                             <TiDelete onClick={() => { this.handleDeleteAnswer(answer.id) }} className="detail-delete-answer"></TiDelete>
-//                             <p className="is-size-7 detail-delete-text">Delete answer</p>
-//                           </div>
-//                         }
-//                       </div>
-//                       <p className="is-size-7 detail-text">{moment(answer.created_at).calendar()}</p>
-//                       <Link className="detail-link-user" to={`/profile/${answer.owner.id}`}><p>{answer.owner.username}</p></Link>
-//                       <div className="detail-text">{parse(answer.text)}</div>
-//                     </div>
-//                   )
-//                 })}
-//               </div>
-//             </>
-//           }
-//           <div className="section">
-//             {Auth.isAuthenticated() ?
-//               <form onSubmit={this.handleSubmit} className="form">
-//                 <SunEditor
-//                   setContents={answerData.submitted ? 'Type your answer here' : ''}
-//                   onChange={this.handleChangeEditor}
-//                   required="True"
-//                   lang="en"
-//                   placeholder="Type your answer here"
-//                   setOptions={{
-//                     height: 200,
-//                     buttonList: [
-//                       ['font', 'fontSize', 'formatBlock'],
-//                       ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
-//                       ['fontColor', 'hiliteColor']
-//                     ]
-//                   }}
-//                 />
-//                 <button className="button detail-button is-warning">Send</button>
-//               </form>
-//               :
-//               <h4>You need to be <Link to='/login'>logged</Link> in to answer this question!</h4>
-//             }
-//           </div>
-//         </div>
-//       </div>
-//     </div >
-//   )
-// }
-// }
 
 export default Detail
--- frontend/src/components/user/Register.js
+++ frontend/src/components/user/Register.js
@@ -1,146 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Select from 'react-select'
 import { post } from 'axios'
 import ImageUpload from '../common/ImageUpload'
 import { options } from '../common/options'
 
-class Register extends React.Component {
-  state = {
-    formData: {
-      username: '',
-      email: '',
-      password: '',
-      password_confirmation: '',
-      image: '',
-      languages: [],
-    },
-    error: ''
+const Register = ({ history }) => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState('')
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData({ ...formData, [name]: value })
+    setError('')
   }
 
-  handleChange = ({ target: { name, value } }) => {
-    const formData = { ...this.state.formData, [name]: value }
-    this.setState({ formData, error: '' })
-  }
-
-  handdleMultiChange = (selected) => {
+  const handleMultiChange = (selected) => {
     const languages = selected ? selected.map(item => item.value) : []
-    const formData = { ...this.state.formData, languages }
-    this.setState({ formData })
+    setFormData({ ...formData, languages })
+    setError('')
   }
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      await post('api/register', this.state.formData)
-      this.props.history.push('/login')
+      await post('api/register', formData)
+      history.push('/login')
     } catch (err) {
-      this.setState({ error: err })
-      console.log(err)
+      setError(err)
+      console.log(error)
     }
   }
 
-  render() {
-    const { formData, error } = this.state
-    return (
-      <div className="section">
-        <div className="columns">
-          <div className="column is-half is-offset-one-quarter">
-            <div className="has-text-centered">
-              <h1 className="title">Sign up</h1>
-            </div>
-            <form onSubmit={this.handleSubmit} className="form">
-              <div className="field">
-                <label className="label">Username</label>
-                <div className="control">
-                  <input
-                    required={true}
-                    className="input"
-                    type="text"
-                    placeholder="Username"
-                    name="username"
-                    onChange={this.handleChange}
-                    value={formData.username} />
-                </div>
-              </div>
-
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control">
-                  <input
-                    required={true}
-                    className="input"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    onChange={this.handleChange}
-                    value={formData.email} />
-                </div>
-              </div>
-
-              <div className="field">
-                <label className="label">Profile picture</label>
-                <div className="control">
-                  {formData.image && <img className="reg-profile-image image is-96x96" src={formData.image} alt="Profile" />}
-                  <ImageUpload
-                    name="imageURL"
-                    handleChange={this.handleChange}
-                    fieldName="image"
-                    labelClassName="my-label-class"
-                    inputClassName="my-input-class"
-                  />
-                </div>
-              </div>
-
-              <div className="field">
-                <label className="label">Language(s)</label>
-                <div className="control">
-                  <Select
-                    options={options}
-                    isMulti
-                    onChange={this.handdleMultiChange}
-                  />
-                </div>
-              </div>
-
-              <div className="field">
-                <label className="label">Password</label>
-                <div className="control">
-                  <input
-                    required={true}
-                    className="input"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    onChange={this.handleChange}
-                    value={formData.password} />
-                </div>
-              </div>
-
-              <div className="field">
-                <label className="label">Password confirmation</label>
-                <div className="control">
-                  <input
-                    required={true}
-                    className="input"
-                    type="password"
-                    placeholder="Password"
-                    name="password_confirmation"
-                    onChange={this.handleChange}
-                    value={formData.password_confirmation} />
-                </div>
-              </div>
-
-              {error && <p className="is-size-7 error-message">Please check if all fields are filled in correctly</p>}
-              <div className="has-text-centered">
-                <button className="button is-warning">Submit</button>
-              </div>
-            </form>
-
+  return (
+    <div className="section">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <div className="has-text-centered">
+            <h1 className="title">Sign up</h1>
           </div>
+          <form onSubmit={handleSubmit} className="form">
+            <div className="field">
+              <label className="label">Username</label>
+              <div className="control">
+                <input
+                  required={true}
+                  className="input"
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  required={true}
+                  className="input"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Profile picture</label>
+              <div className="control">
+                {formData.image && <img className="reg-profile-image image is-96x96" src={formData.image} alt="Profile" />}
+                <ImageUpload
+                  name="imageURL"
+                  handleChange={handleChange}
+                  fieldName="image"
+                  labelClassName="my-label-class"
+                  inputClassName="my-input-class"
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Language(s)</label>
+              <div className="control">
+                <Select
+                  options={options}
+                  isMulti
+                  onChange={handleMultiChange}
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  required={true}
+                  className="input"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Password confirmation</label>
+              <div className="control">
+                <input
+                  required={true}
+                  className="input"
+                  type="password"
+                  placeholder="Password"
+                  name="password_confirmation"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            {error && <p className="is-size-7 error-message">Please check if all fields are filled in correctly</p>}
+            <div className="has-text-centered">
+              <button className="button is-warning">Submit</button>
+            </div>
+          </form>
         </div>
-      </div >
-    )
-  }
+      </div>
+    </div >
+  )
 }
 
 export default Register
